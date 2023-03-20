730f62b8f1cc7b9b5c489be60fee74f1878fbdc9
OliLoftus <oliverloftus1@gmail.com>
2023-03-20 15:19:47 +0000

    adjust image size and responsiveness on landingPage

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -5,8 +5,18 @@ export default function LandingPage() {
   return (
     <div className="flex flex-col justify-center items-center">
       <Navbar />
-      <h1 className="text-4xl font-bold mb-6">RunWay</h1>
-      <img src={landingPageImage} alt="animated man and globe" />
+      <h1 className="text-4xl font-bold mb-6 text-center text-gray-900">
+        RunWay
+      </h1>
+      <img
+        src={landingPageImage}
+        alt="animated man and globe"
+        className="w-96 h-96 object-cover rounded-md mb-6"
+      />
+      <p className="text-lg text-center text-gray-700 mb-6">
+        Welcome to Runway! Ready to travel smarter, not harder? Tell us your
+        travel budget and we'll show you where you can go. Let's explore!
+      </p>
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Prepare for takeoff!
       </button>
