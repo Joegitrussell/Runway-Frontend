894214874cbea4521c68b71993867798fc52ca06
OliLoftus <oliverloftus1@gmail.com>
2023-03-20 15:04:44 +0000

    changed css on landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -3,7 +3,7 @@ import landingPageImage from "../../assets/images/RunWay_ob1.png";
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col justify-center items-center md:flex-row md:justify-between">
+    <div className="flex flex-col justify-center items-center">
       <Navbar />
       <h1 className="text-4xl font-bold mb-6">RunWay</h1>
       <img src={landingPageImage} alt="animated man and globe" />
