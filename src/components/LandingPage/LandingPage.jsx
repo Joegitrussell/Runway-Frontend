ed76d17d5953abefb0432749c9cae2e50532327b
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-03-28 20:50:23 +1100

    28/03/2023 Commit Landing Page design adjusted, Taylor, Oli and Joe

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -17,21 +17,29 @@ export default function LandingPage() {
   console.log("Fuck off")
 
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center md:container d-mx-auto">
       <Navbar />
       <img src={Runway} alt="runway-header" className="w-3/4 mt-6" />
+
       <img
         src={landingPageImage}
         alt="animated man and globe"
         className="w-96 h-96 object-cover rounded-md mb-6"
       />
-      <p className="text-lg text-center text-gray-700 mb-6 font-sans-serif font-bold italic subpixel-antialiased">
+      
+      <p className="text-lg text-center text-gray-700 mb-6 font-sans-serif font-bold italic subpixel-antialiased px-8 w-3/4">
         Welcome to Runway! Ready to travel smarter, not harder? Tell us your
         travel budget and we'll show you where you can go. Let's explore!
       </p>
-      <button className="btn mb-20" onClick={handleTakeoffClick}>Prepare for takeoff!</button>
-      <BudgetPage></BudgetPage>
-
+      <div className="flex flex-col justify-center items-center gap-5">
+        <h2 className="text-3xl col text-black font-style: italic p-2">What's your budget?</h2>
+        <input 
+          className="bg-white text-black rounded-lg w-64 h-12 p-2" 
+          type="text" 
+          placeholder="£3000"
+        />
+        <button className="btn mb-20 bg-secondary text-black w-64 h-12 font-style: italic" onClick={handleTakeoffClick}>Prepare for takeoff!</button>
+      </div>
     </div>
   );
 }
