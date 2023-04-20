f4e0a49a26a6ebd66b7ca75cb41602d37c018d45
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-04-20 21:07:31 +1000

    Taylor and Oli, Images added to data object

--- src/data/locationData.js
+++ src/data/locationData.js
@@ -6,13 +6,13 @@ export const locations = {
     peak: 100,
   },
   London: {
-    url: "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80",
+    url: "https://images.unsplash.com/photo-1520986606214-8b456906c813?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     base: 60,
     mid: 90,
     peak: 120,
   },
   NewYork: {
-    url: "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80",
+    url: "https://images.unsplash.com/photo-1492666673288-3c4b4576ad9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80",
     base: 70,
     mid: 105,
     peak: 140,
