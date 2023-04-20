b3f569768943cad3a0b7a95cab8f460e39fff789
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-04-20 21:32:12 +1000

    Taylor commit

--- src/components/ResultsPage/ResultsPage.jsx
+++ src/components/ResultsPage/ResultsPage.jsx
@@ -23,14 +23,16 @@ export default function ResultsPage() {
       <h1>Trips that fit!</h1>
       <h2>Your budget is: {budget}</h2>
       {Object.entries(locations).map(([location, image]) => (
-        <div key={location} className="card card-compact w-96 bg-base-100 shadow-xl">
-          <h3 className="card-title">{location}</h3>
-          <img src={image.url} alt="test" />
-          <ul className="card-actions justify-end">
-            <li>Base: {stayDuration[location].base} days</li>
-            <li>Mid: {stayDuration[location].mid} days</li>
-            <li>Peak: {stayDuration[location].peak} days</li>
-          </ul>
+        <div className="container mx-auto">
+          <div key={location} className="card card-compact w-96 bg-base-100 shadow-xl">
+            <h3 className="card-title">{location}</h3>
+            <img src={image.url} alt="test" />
+            <ul className="card-actions justify-end">
+              <li>Base: {stayDuration[location].base} days</li>
+              <li>Mid: {stayDuration[location].mid} days</li>
+              <li>Peak: {stayDuration[location].peak} days</li>
+            </ul>
+          </div>
         </div>
       ))}
     </>
