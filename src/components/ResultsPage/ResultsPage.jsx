a0b16be4685026238175aec82f834c13ae032845
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-04-20 21:04:11 +1000

    Taylor and Oli commit, sorted map, which now shows the images from the dummy data

--- src/components/ResultsPage/ResultsPage.jsx
+++ src/components/ResultsPage/ResultsPage.jsx
@@ -22,10 +22,10 @@ export default function ResultsPage() {
     <>
       <h1>Trips that fit!</h1>
       <h2>Your budget is: {budget}</h2>
-      {Object.entries(locations).map(([location, prices]) => (
+      {Object.entries(locations).map(([location, image]) => (
         <div key={location} className="card card-compact w-96 bg-base-100 shadow-xl">
           <h3 className="card-title">{location}</h3>
-          <img src="https://images.unsplash.com/photo-1502602898657-3e91760cbb34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80" alt="test" />
+          <img src={image.url} alt="test" />
           <ul className="card-actions justify-end">
             <li>Base: {stayDuration[location].base} days</li>
             <li>Mid: {stayDuration[location].mid} days</li>
--- src/data/locationData.js
+++ src/data/locationData.js
@@ -1,15 +1,18 @@
 export const locations = {
   Paris: {
+    url: "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80",
     base: 50,
     mid: 75,
     peak: 100,
   },
   London: {
+    url: "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80",
     base: 60,
     mid: 90,
     peak: 120,
   },
   NewYork: {
+    url: "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80",
     base: 70,
     mid: 105,
     peak: 140,
