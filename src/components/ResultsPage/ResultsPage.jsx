73c411f64ebadd3d41e493892dce7b46e9cc6234
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-04-23 20:35:09 +1000

    Oli, Gabs, Taylor Commit: Card state updated to show Base Mid and Peak, for slider, need to add design

--- src/components/ResultsPage/ResultsPage.jsx
+++ src/components/ResultsPage/ResultsPage.jsx
@@ -1,9 +1,8 @@
+import { useState } from "react";
 import { locations } from "../../data/locationData.js";
-import Paris from "../../assets/images/paris.jpg"
-
+import Paris from "../../assets/images/paris.jpg";
 export default function ResultsPage() {
   const budget = parseInt(localStorage.getItem("budget"));
-
   const calculateStayDuration = (budget) => {
     const stayDuration = {};
     Object.keys(locations).forEach((location) => {
@@ -11,13 +10,21 @@ export default function ResultsPage() {
         base: Math.floor(budget / locations[location].base),
         mid: Math.floor(budget / locations[location].mid),
         peak: Math.floor(budget / locations[location].peak),
+        // selected: Math.floor(budget / locations[location].base)
       };
     });
     return stayDuration;
   };
-
-  const stayDuration = calculateStayDuration(budget);
-
+  const [stayDuration, setStayDuration] = useState(calculateStayDuration(budget));
+  const handleSliderChange = (event, location) => {
+    setStayDuration((prevState) => ({
+      ...prevState,
+      [location]: {
+        ...prevState[location],
+        selected: parseInt(event.target.value)
+      }
+    }));
+  };
   return (
     <>
       <h1>Trips that fit!</h1>
@@ -26,12 +33,29 @@ export default function ResultsPage() {
         <div className="container mx-auto">
           <div key={location} className="card card-compact w-96 bg-base-100 shadow-xl">
             <h3 className="card-title">{location}</h3>
-            <img src={image.url} alt="test" />
+            <img src={image.url} alt={location} />
             <ul className="card-actions justify-end">
               <li>Base: {stayDuration[location].base} days</li>
               <li>Mid: {stayDuration[location].mid} days</li>
               <li>Peak: {stayDuration[location].peak} days</li>
             </ul>
+            <div className="mt-4">
+              <input
+                type="range"
+                min="0"
+                max={budget}
+                value={stayDuration[location].selected}
+                className="range"
+                step={locations[location].base}
+                onChange={(event) => handleSliderChange(event, location)}
+              />
+              <div className="w-full flex justify-between text-xs px-2">
+                <span>{stayDuration[location].base}</span>
+                <span>{stayDuration[location].mid}</span>
+                <span>{stayDuration[location].peak}</span>
+              </div>
+              <div className="mt-2">Selected: {stayDuration[location].selected} days</div>
+            </div>
           </div>
         </div>
       ))}
