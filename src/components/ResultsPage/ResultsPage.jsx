08544b51859dd33bd49d901c6863498e54abbe01
OliLoftus <oliverloftus1@gmail.com>
2023-05-15 20:59:41 +0100

    deleted noise, changed data to an array and altered map accordingly

--- src/components/ResultsPage/ResultsPage.jsx
+++ src/components/ResultsPage/ResultsPage.jsx
@@ -1,77 +1,57 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { locations } from "../../data/locationData.js";
 
 export default function ResultsPage() {
   const budget = parseInt(localStorage.getItem("budget"));
-
-  // const calculateStayDuration = (budget) => {
-  //   const stayDuration = {};
-  //   Object.keys(locations).forEach((location) => {
-  //     stayDuration[location] = {
-  //       base: Math.floor(budget / locations[location].base),
-  //       mid: Math.floor(budget / locations[location].mid),
-  //       peak: Math.floor(budget / locations[location].peak),
-  //     };
-  //   });
-  //   return stayDuration;
-  // };
-
-   const [stayDuration, setStayDuration] = useState({selected: 0});
-
-  // useEffect(() => {
-  //   setStayDuration(calculateStayDuration(budget));
-  // }, []);
-
-  const handleSliderChange = (event, location) => {
-    setStayDuration((prevState) => ({
-      ...prevState,
-      [location]: {
-        ...prevState[location],
-        selected: parseInt(event.target.value)
-        
-      }
-    }));
-    console.log(stayDuration)
-  };
+  const [stayDuration, setStayDuration] = useState({});
 
   return (
     <>
       <h1>Trips that fit!</h1>
       <h2>Your budget is: {budget}</h2>
       <div className="flex flex-wrap justify-center">
-        {Object.entries(locations).map(([location, data]) => (
-          <div key={location} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 p-2">
+        {locations.map((location) => (
+          <div
+            key={location.name}
+            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 p-2"
+          >
             <div className="card lg:card-side bg-base-100 shadow-xl">
-              <h2 className="card-title">{location}</h2>
+              <h2 className="card-title">{location.name}</h2>
               <div className="relative">
-                    <figure>
-                      <img src={data.url} alt={location} className="object-cover w-full" />
-                    </figure>
-                    {stayDuration[location] && (
-                      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white px-2 py-1">
-                        <div className="flex flex-col items-center justify-center text-center">
-                          <div className="text-7xl">
-                            {Math.round(budget/(stayDuration[location].selected))}
-                          </div>
-                          <div>
-                            Days
-                          </div>
-                        </div>
+                <figure>
+                  <img
+                    src={location.url}
+                    alt={location.name}
+                    className="object-cover w-full"
+                  />
+                </figure>
+                {stayDuration[location.name] && (
+                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white px-2 py-1">
+                    <div className="flex flex-col items-center justify-center text-center">
+                      <div className="text-7xl">
+                        {Math.round(
+                          budget / stayDuration[location.name].selected
+                        )}
                       </div>
-                    )}
+                      <div>Days</div>
+                    </div>
                   </div>
+                )}
+              </div>
               <div className="card-body">
-                {/* <h2 className="card-title">{location}</h2> */}
-              
                 <div className="card-actions justify-end">
                   <input
                     type="range"
-                    min={data.base}
-                    max={data.peak}
-                    value={stayDuration[location]?.selected || 0}
+                    min={location.base}
+                    max={location.peak}
+                    value={
+                      stayDuration[location.name]?.selected || location.mid
+                    }
                     className="range"
-                    step={data.base/2}
-                    onChange={(event) => handleSliderChange(event, location)}
+                    step={location.base / 2}
+                    onChange={(event) =>
+                      handleSliderChange(event, location.name)
+                    }
                   />
                   <div className="w-full flex justify-between text-xs px-2">
                     <span>Base</span>
--- src/data/locationData.js
+++ src/data/locationData.js
@@ -1,20 +1,23 @@
-export const locations = {
-  Paris: {
+export const locations = [
+  {
+    name: "Paris",
     url: "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80",
     base: 50,
     mid: 75,
     peak: 100,
   },
-  London: {
+  {
+    name: "London",
     url: "https://images.unsplash.com/photo-1520986606214-8b456906c813?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     base: 60,
     mid: 90,
     peak: 120,
   },
-  NewYork: {
+  {
+    name: "NewYork",
     url: "https://images.unsplash.com/photo-1492666673288-3c4b4576ad9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80",
     base: 70,
     mid: 105,
     peak: 140,
   },
-};
+];
