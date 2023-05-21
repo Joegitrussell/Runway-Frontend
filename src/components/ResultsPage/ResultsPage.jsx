00ef6a6bf35d2712cf911e154e954d812744587f
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-05-21 18:54:58 +1000

    Taylor and Gabs, seccond design commit

--- src/components/ResultsPage/ResultsPage.jsx
+++ src/components/ResultsPage/ResultsPage.jsx
@@ -22,13 +22,13 @@ export default function ResultsPage() {
       <h2 className="font-custom text-white">Your budget is: {budget}</h2>
       <div className="flex flex-wrap justify-center">
         {Object.entries(locations).map(([location, data]) => (
-          <div key={location} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 p-2">
+          <div key={location} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 p-4">
             <div className="card lg:card-side bg-base-100 shadow-xl rounded-t-lg">
               <div className="relative">
                 <figure className="rounded-tl-lg rounded-tr-lg overflow-hidden">
                   <img src={data.url} alt={location} className="object-cover w-full" />
                 </figure>
-                <div className="absolute top-0 left-0 p-10">
+                <div className="absolute top-0 left-0 p-4">
                   <h2
                     className="card-title f text-white font-custom"
                     style={{
@@ -36,9 +36,9 @@ export default function ResultsPage() {
                       width: "122px",
                       height: "29px",
                       left: "12px",
-                      top: "0px",
+                      top: "8px",
                       fontWeight: 1000,
-                      fontSize: "24px",
+                      fontSize: "20px",
                       lineHeight: "29px",
                       color: "#FFFFFF",
                       textShadow: "4px 4px 4px #000000",
@@ -49,12 +49,13 @@ export default function ResultsPage() {
                   </h2>
                 </div>
                 {stayDuration[location] && (
-                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white px-2 py-1">
+                  <div className="absolute inset-0 flex items-center justify-center  text-white px-2 py-1">
                     <div className="flex flex-col items-center justify-center text-center">
-                      <div className="text-7xl">
+                      <div className="text-7xl" style={{fontWeight: 450, textShadow: "4px 4px 4px #000000", WebkitTextStroke: "1px #000000"}}>
                         {Math.round(budget / stayDuration[location].selected)}
                       </div>
-                      <div>Days</div>
+                      <div style={{ fontWeight: 900,
+                      fontSize: "20px", textShadow: "4px 4px 4px #000000", WebkitTextStroke: "1px #000000", lineHeight: "29px",}}>Days</div>
                     </div>
                   </div>
                 )}
